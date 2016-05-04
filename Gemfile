f90882d61d3a701b880e71d159ce14cfe1b619f0
evelin <eponce19@gmail.com>
2016-05-04 11:10:57 -0500

    gemfile

--- Gemfile
+++ Gemfile
@@ -16,7 +16,8 @@ gem 'coffee-rails', '~> 4.1.0'
 
 gem 'bootstrap-sass', '~> 3.2.0'
 gem 'autoprefixer-rails'
-
+gem 'moodle-api'
+gem 'pry'
 
 # Use jquery as the JavaScript library
 gem 'jquery-rails'
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,21 +2,17 @@
   class WelcomeController < ApplicationController
     def index
 
-      host = 'http://evafisca.com/learn'
+      host = 'http://evafisica.com/learn'
 
       configuration          = Moodle::Api::Configuration.new
       configuration.host     = host
       configuration.username = 'kendra'
       configuration.password = 'Kendra2016#'
       configuration.service  = 'eva-prac-2016'
-      p configuration
-      p host
-      @configuration = configuration
       token =  Moodle::Api::TokenGenerator.new(configuration).call
       # The client can also be instantiated and used.
-      p token
-
-rescue
+      #p token
+ rescue
     @message = "No se genero token"
 
 end
