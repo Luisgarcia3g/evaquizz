10cd852f20c06ec31310e0bb2a8a5c57df8d1c0f
evelin <eponce19@gmail.com>
2016-06-04 11:54:45 -0500

    Fix unique lenght in migration

--- db/migrate/20160526194827_add_indextotemas.rb
+++ db/migrate/20160526194827_add_indextotemas.rb
@@ -1,5 +1,5 @@
 class AddIndextotemas < ActiveRecord::Migration
   def change
-     add_index :temas, :nombretema, unique: true
+     add_index :temas, :nombretema, unique: true, length: 767
   end
 end
