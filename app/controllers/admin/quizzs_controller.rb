bd7692adc019173ac95b47cbc69fcc3339ffd455
evelin <eponce19@gmail.com>
2016-06-04 12:46:15 -0500

    Agregar identacion

--- app/controllers/admin/quizzs_controller.rb
+++ app/controllers/admin/quizzs_controller.rb
@@ -3,49 +3,43 @@ class Admin::QuizzsController < ApplicationController
   def index
     @quizzes = Quizz.all.paginate(page: params[:page], per_page: 10).order('temaid ASC')
     @temas=Tema.all
-
   end
 
-def new
- @tema_options = Tema.all.map{ |t| [ t.nombretema, t.id ] }
+  def new
+    @tema_options = Tema.all.map{ |t| [ t.nombretema, t.id ] }
     @preguntas = Pregunta.all
-  @path_prefix = :admin
-  @quizz = Quizz.new
-end
-
-def show
-
-    @quizz = Quizz.find(params[:id])
-      @temas=Tema.all
-end
-
-def create
-  @quizz = Quizz.new(quizz_params)
-  if @quizz.save
-    redirect_to :action => :show, :id => @quizz.id
-  else
-    render :new
+    @path_prefix = :admin
+    @quizz = Quizz.new
   end
 
+  def show
+    @quizz = Quizz.find(params[:id])
+    @temas=Tema.all
+  end
 
-end
+  def create
+    @quizz = Quizz.new(quizz_params)
+    if @quizz.save
+      redirect_to :action => :show, :id => @quizz.id
+    else
+      render :new
+    end
+  end
 
-def edit
+  def edit
+    @quizz = Quizz.find(params[:id])
+    @tema_options = Tema.all.map{ |t| [ t.nombretema, t.id ] }
+    @preguntas = Pregunta.all
+  end
 
-@quizz = Quizz.find(params[:id])
-@tema_options = Tema.all.map{ |t| [ t.nombretema, t.id ] }
-   @preguntas = Pregunta.all
-end
-
-def update
-  @quizz = Quizz.find(params[:id])
-
-   if @quizz.update_attributes(quizz_params)
-     redirect_to :action => :show, :id =>@quizz.id
-   else
-     render :edit
-   end
-end
+  def update
+    @quizz = Quizz.find(params[:id])
+    if @quizz.update_attributes(quizz_params)
+      redirect_to :action => :show, :id =>@quizz.id
+    else
+      render :edit
+    end
+  end
 
 private
   def quizz_params
@@ -53,7 +47,6 @@ private
     #de lo contrario me da el error de Pregunta expected, got String
    params.require(:quizz).permit(:nombre, :disponible, :temaid, :preguntas_ids => [])
   end
-
 
 
 end
