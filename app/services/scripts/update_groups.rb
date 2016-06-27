30d6ca4aeb5b585b192141b4dfe7b4a0f1d285c5
eponce19 <eponce19@gmail.com>
2016-06-27 13:11:15 -0500

    Update service

--- app/services/scripts/update_groups.rb
+++ app/services/scripts/update_groups.rb
@@ -1,10 +1,10 @@
-# class Scripts::UpdateGroups
-#   include Service
-#
-#   def initialize()
-#   end
-#
-#   def call
+class Scripts::UpdateGroups
+  include Service
+
+  def initialize()
+  end
+
+  def call
     # create non-erasable folders for lessons and side-quests
     params = { 'criteria[0][key]' => 'courseid', 'criteria[0][value]' => '%%' }
     @courses = Moodle::Api.core_enrol_get_enrolled_users(params)
@@ -24,6 +24,6 @@
     # Resource.all.each do |resource|
     #   resource.resource_assets.create(uploaded_file: resource.uploaded_file, is_for_teacher: false)
     # end
-#   end
-#
-# end
+  end
+
+end
