694222ab4d99876b251b711f6f4bee85cd443dce
Luisgarcia3g <luis.garciag@udem.edu>
2017-01-06 10:26:25 -0600

    PARA HEROKU

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
+  #allala
   root 'welcome#index'
 
   get '/welcome', to: 'welcome#index'
